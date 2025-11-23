1b17879e616035d5d72228b6602899f7c50a24da
pepelinlin <pelin.karadal@sabanciuniv.edu>
2025-11-23 23:43:59 +0300

    Add files via upload

---
+++ HW3/analysis_part2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:/Users/suuser/Desktop/IoT_HW3/latency_part2.csv")
+
+df['total_latency'] = df['inference_time'] + df['rtt_seconds']
+avg_latency = df['total_latency'].dropna().mean()
+print(f"Average latency (s): {avg_latency:.3f}")
+
+df['fps'] = 1 / df['inference_time']
+max_fps = df['fps'].max()
+print(f"Maximum inference rate (FPS): {max_fps:.2f}")
+
+timestamps = df['capture_ts'].to_numpy()
+frame_deltas = np.diff(timestamps)  # Time between consecutive frames
+fps_estimate = 1 / np.mean(frame_deltas)  # Average FPS end-to-end
+max_fps_end2end = 1 / np.min(frame_deltas)  # Maximum FPS observed
+print(f"Average end-to-end FPS: {fps_estimate:.2f}")
+print(f"Maximum end-to-end FPS: {max_fps_end2end:.2f}")
