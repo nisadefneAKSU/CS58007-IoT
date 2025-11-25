e2bc9f09fc4fa0d7f71b3690714ca18fc173c188
Shahd628 <shahd.shehab.sherif@gmail.com>
2025-11-26 02:06:45 +0300

    Updated analysis.py name

---
+++ HW3/analysis_part1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import os.path
+
+df = pd.read_csv("device/latency_log_part1.csv")
+avg_latency = df['server_latency'].dropna().mean()
+print(f"Average latency (s): {avg_latency:.2f}") # This gives the average latency per frame
+
+# df['fps'] = 1 / df['inference_time']
+max_fps = df['current_fps'].max()
+print(f"Maximum inference rate (FPS): {max_fps:.2f}")
+
+timestamps = df['capture_ts'].to_numpy()
+frame_deltas = np.diff(timestamps)  # Time between consecutive frames
+fps_estimate = 1 / np.mean(frame_deltas)  # Average FPS end-to-end
+max_fps_end2end = 1 / np.min(frame_deltas)  # Maximum FPS observed
+print(f"Average end-to-end FPS: {fps_estimate:.2f}")
+print(f"Maximum end-to-end FPS: {max_fps_end2end:.2f}")
